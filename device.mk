3527b9a9ad0e1c3760afe4182b5066cedb4c3e30
Koushik Dey <kousickdey3@gmail.com>
2025-07-08 23:13:51 +0530

    beryllium: Disable Matlog
    * Disable Matlog if the build variant is 'user' to prevent log access in production builds
    
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Change-Id: I1d4a0ecdba1f029340e3ca2486e7a663a58c753b

--- device.mk
+++ device.mk
@@ -80,6 +80,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
+# Matlog
+ifeq ($(TARGET_BUILD_VARIANT),user)
+TARGET_DISABLE_MATLOG := true
+endif
+
 # PocketMode
 PRODUCT_PACKAGES += \
     XiaomiPocketMode
